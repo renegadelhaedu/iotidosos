797a095051d89f8eb6940035e7aae9131589eadf
renegadelhaedu <renegadelha.edu@gmail.com>
2025-09-22 16:22:43 -0300

    v5 debug off

--- server.py
+++ server.py
@@ -69,5 +69,5 @@ if __name__ == '__main__':
     socketio.run(app,
                  #host='0.0.0.0',
                  port=5000,
-                 debug=True,
+                 debug=False,
                  allow_unsafe_werkzeug=True)
